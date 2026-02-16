47f948e17daeae064b8c6742b42c795c51d66419
madlings <nagalingam.s@gmail.com>
2026-02-16 21:23:01 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,6 +41,10 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        BackendDeveloper.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
